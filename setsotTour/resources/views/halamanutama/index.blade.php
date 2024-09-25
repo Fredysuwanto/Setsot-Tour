19e5d2a6c9636565102763374a6ce7ac863d5003
Fredysuwanto <fredysuwanto56@gmail.com>
2024-09-25 22:32:05 +0700

    push

--- setsotTour/resources/views/halamanutama/index.blade.php
+++ setsotTour/resources/views/halamanutama/index.blade.php
@@ -131,6 +131,21 @@
                                         <span>Tour Guide</span>
                                     </div>
                             </div>
+                            <div class="col-lg-3">
+                                <img src="https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611716.jpg?size=626&ext=jpg&ga=GA1.2.708301025.1727177859&semt=ais_hybrid" class="img-fluid" alt="" />
+                                    <div class="text-center">
+                                        <h3>Danang</h3>
+                                        <span>Tour Guide</span>
+                                    </div>
+                                </div>
+                            <div class="col-lg-3">
+                                <img src="https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611753.jpg?size=626&ext=jpg" class="img-fluid" alt="" />
+                                    <div class="text-center">
+                                        <h3>Rismanto</h3>
+                                        <span>Tour Guide</span>
+                                    </div>
+                                </div>
+                            
                         </div>
                     </div>
                 </div>

5d84a49294347ef7c9670d196644456a2bfe0e99
Wulann <komariahwulandari_2226240119@mhs.mdp.ac.id>
2024-09-25 21:37:05 +0700

    update index blade
    mengedit index blade

--- resources/views/registrasi/index.blade.php
+++ resources/views/registrasi/index.blade.php
@@ -15,43 +15,7 @@
         }
     </style>
 </head>
-<body>
-    <div class="container">
-        <h1 class="my-4 text-center">Daftar Registrasi</h1>
-        <a href="{{ route('registrasi.create') }}" class="btn btn-primary col-lg-12">Buat Pemesanan Baru</a>
-        <hr>
-        <div class="row">
-            @foreach ($registrasi as $item)
-                <div class="col-md-4 mb-4 ">
-                    <div class="card bg-dark">
-                        <div class="card-body ">
-                            <h1 class="card-title text-center text-light">{{ $item['tour'] }}</h1>
-                            <img src="{{  url('assets/img/kaiadmin/qrcode.png') }}" class="card-img-top rounded">
-                            <p class="card-text text-center text-light">{{ $item['nama'] }}</p>
-                            <p class="card-text text-center text-light">{{ $item['waktu'] }}</p>
-                            <p class="card-text text-center text-light">{{ $item['tanggal'] }}</p>
-                            <p class="card-text text-center text-light">{{ $item['harga'] }}</p>
-                            <div class="text-center p-1">
-                                <form action="{{route('registrasi.destroy', $item["id"])}}" method="post">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button type="submit" class="btn btn-danger btn-sm show_confirm col-lg-3">Delete</button>
-                                </form>
-                            </div>
-                            <div class="text-center">
-                                <a href="{{route('registrasi.edit', $item["id"])}}" class="btn btn-sm btn-warning col-lg-3">Edit</a>
-                            </div>
-                            <div class="text-center p-1">
-                                <a href="{{route('registrasi.show', $item["id"])}}" class="btn btn-sm btn-warning btn-rounded col-lg-7">Confirm Pembayaran</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
-    <script src="{{ url('js/app.js') }}"></script>
-</body>
+
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 @if (session('success'))

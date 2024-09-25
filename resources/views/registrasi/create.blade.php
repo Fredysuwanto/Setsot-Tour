3a3a9302528b88abb39e93fe3118a44b11a2355e
Wulann <komariahwulandari_2226240119@mhs.mdp.ac.id>
2024-09-25 21:25:47 +0700

    Register - Create 2
    menambahkan jenis tour yang dapat dipilih

--- resources/views/registrasi/create.blade.php
+++ resources/views/registrasi/create.blade.php
@@ -10,6 +10,34 @@
         <p class="card-description d-flex justify-content-center align-items-center">
           > Masukan Data anda <
         </p>
+        <form method="POST" action="{{ route('registrasi.store') }}" class="forms-sample" enctype="multipart/form-data">
+          @csrf
+          <div class="form-group">
+            <label for="nama" class="text-light">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama" value="{{ old('nama') }}" placeholder="Masukan Nama Anda">
+            @error('nama')
+                <span class="text-danger">{{$message}}</span>
+            @enderror
+        </div>
+        <div class="form-group">
+          <label for="tour" class="text-light">Tour</label>
+          <select type="text" class="form-control" id="tour" name="tour" placeholder="Masukan Tour di inginkan">
+              <option value="Tour Jepang"> Tour Jepang </option>
+              <option value="Tour Labuan Bajo"> Tour Labuan Bajo</option>
+              <option value="Tour Palembang"> Tour Palembang</option>
+              <option value="Tour Bali"> Tour Bali</option>
+              <option value="Tour Palembang"> Tour Palembang</option>
+              <option value="Tour madura"> Tour Madura</option>
+              <option value="Tour Maluku"> Tour Maluku</option>
+              <option value="Tour Medan"> Tour Medan</option>
+              <option value="Tour Bandung"> Tour Bandung</option>
+            </select>
+          @error('tour')
+              <span class="text-danger">{{$message}}</span>
+          @enderror
+        </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
